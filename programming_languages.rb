78a8f2dc325724d727ea8ca1b7eddb9fa92ed36b
Jathmel Gordon <jathmel@gmail.com>
2018-05-11 18:20:50 +0000

    automatically backed up by learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -31,17 +31,7 @@ def style(languages)
 end
 
 
-def reformat_languages(languages)
-  new_languages = {}
-  languages.each do |type, hash|
-    hash.each do |name, attributes|
-      new_languages[name] ||= attributes
-      new_languages[name][:style] ||= []
-      new_languages[name][:style] << type
-    end
-  end
-  new_languages
-end
+
 
 
 

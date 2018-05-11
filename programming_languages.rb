622fb14e6c2be394b9f84964d3630f93373cf524
Jathmel Gordon <jathmel@gmail.com>
2018-05-11 17:44:13 +0000

    automatically backed up by learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,10 +1,14 @@
 def reformat_languages(languages)
   new_hash = language(languages)
-  # your code here
+  languages.each do |style, info|
+    info.each do |type, val|
+      
+    
+  
     
 end
 
-def language(data)
+def language(langauges)
   new_hash = Hash.new { |hash, key| hash[key] = Hash.new(){[]} }
   data.each_pair do |style, lang|
     lang.each_pair do |key, val|

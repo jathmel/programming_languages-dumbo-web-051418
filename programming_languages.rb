7c15dca552e55947118c96ec14ff4f286baa4802
Jathmel Gordon <jathmel@gmail.com>
2018-05-11 18:39:07 +0000

    automatically backed up by learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,28 +1,5 @@
-def reformat_languages(languages)
-  new_hash = language(languages)
-  languages.each do |style, info|
-    info.each do |type, val|
-      val.each do |lang, att|
-        new_hash[lang][:style] = style 
-      end
-    end
-  end
-  new_hash
-      
-    
-  
-    
-end
 
-def language(hash)
-  new_hash = {}#Hash.new { |hash, key| hash[key] = {} }
-  hash.each_pair do |style, lang|
-    lang.each_pair do |key, val|
-      new_hash[key] ={}
-    end
-  end
-  new_hash
-end
+
 
 
 
